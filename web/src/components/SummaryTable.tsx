6d168ec627512b45206f6ef4db6a3b0a8d08b8db
Wallysson <wlc.couto@gmail.com>
2023-01-21 11:57:18 -0300

    Class3: <create>BarProgress, ModalCreateNewHabits and css in habits completed

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -27,7 +27,13 @@ export function SummaryTable() {
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date, index) => {
-          return <HabitDay key={date.toString()} />
+          return (
+            <HabitDay
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+              key={date.toString()}
+            />
+          )
         })}
 
         {amountOfDaysToFill > 0 &&
